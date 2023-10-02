ecc907f1d2c65141f59f38ea6be965ec33ca86fc
IT20154080 <IT20154080@my.sliit.lk>
2023-10-02 19:04:48 +0530

    fix DOS attack

--- frontend/src/Components/eventScheduling/AddEvents.js
+++ frontend/src/Components/eventScheduling/AddEvents.js
@@ -148,7 +148,12 @@ const AddEvents = ({ toggle, setToggle }) => {
                                     }
                                     return value && SUPPORTED_FORMATS.includes(value?.type)
                                 }
-                            ),
+                            )
+                            .test(
+                                'fileSize',
+                                'File size must be less than 10MB',
+                                (value) => value && value.size <= 10000000
+                            )
                     })}
                     onSubmit={(values, { setSubmitting }) => {
                         console.log('values.date', values.date)
